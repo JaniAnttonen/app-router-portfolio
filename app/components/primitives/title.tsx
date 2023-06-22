cd9a92a80439979777775e455a9ad2766ac4563d
JaniAnttonen <jani.anttonen@protonmail.ch>
2023-06-22 15:06:37 +0300

    Fix title vertical alignment

--- app/components/primitives/title.tsx
+++ app/components/primitives/title.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 
 const Title = (props: TitleProps) => {
   return (
-    <div className="flex flex-row mt-6 mb-3 flex-nowrap overflow-x-hidden">
+    <div className="flex flex-row items-center mt-6 mb-3 flex-nowrap overflow-x-hidden">
       <h1 className="flex flex-row items-center gap-1 flex-shrink-0">{props.children}</h1>
       <Image src="/title.png" width={440} height={20} alt={"title image"} className="mix-blend-difference dark:mix-blend-normal flex flex-shrink-0" />
     </div>
