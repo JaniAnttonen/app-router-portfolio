2a8cc6b0886e80a777d9ecdc4aef697605baf2bf
Jani Anttonen <jani.anttonen@protonmail.ch>
2025-02-06 23:01:10 +0200

    Update intro.tsx

--- app/components/sections/intro.tsx
+++ app/components/sections/intro.tsx
@@ -8,7 +8,7 @@ const Introduction = () => {
         <Image src="/avatar.jpg" width={120} height={120} alt={"avatar"} className="mix-blend-luminosity appicon" />
         <div className="flex flex-col justify-evenly">
           <h1 className="text-2xl font-bold">Jani Anttonen</h1>
-          <p className="text-lg">Interim CTO at <a href="https://www.treeprotocol.xyz/" target="_blank">TREE Protocol</a> creating <a href="https://hadea.ec.europa.eu/calls-proposals/digital-product-passport_en" title="Digital Product Passport" target="_blank">DPPs</a></p>
+          <p className="text-lg">Software Engineer at <a href="https://www.treeprotocol.xyz/" target="_blank">TREE Protocol</a> creating <a href="https://hadea.ec.europa.eu/calls-proposals/digital-product-passport_en" title="Digital Product Passport" target="_blank">DPPs</a></p>
           <div className="flex flex-row gap-3 flex-wrap">
             <ContactInfo>Email: jani.anttonen@protonmail.ch</ContactInfo>
           </div>
