eafd0da4221a271d5cab049c17bc5d018adce9ac
JaniAnttonen <jani.anttonen@protonmail.ch>
2025-10-20 16:07:25 +0300

    fontstuf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Jani Anttonen – Software Engineer',
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
       {/* TODO: Add a footer with links to legacy sites */}
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
+import localFont from 'next/font/local'
 import Introduction from './components/sections/intro'
 import Links from './components/sections/links'
 import Mirror from './components/sections/mirror'
 import Presentations from './components/sections/presentations'
 import Projects from './components/sections/projects'
 
+const therma = localFont({ src: [
+  { 
+    path: '../public/fonts/ASTherma-MediumCondensed.woff',
+    weight: '500',
+    style: 'normal',
+  },
+  { 
+    path: '../public/fonts/ASTherma-LightCondensed.woff',
+    weight: '400',
+    style: 'normal',
+  },
+  { 
+    path: '../public/fonts/ASTherma-BoldCondensed.woff',
+    weight: '700',
+    style: 'normal',
+  },
+  { 
+    path: '../public/fonts/ASTherma-BlackCondensed.woff',
+    weight: '900',
+    style: 'normal',
+  },
+]})
+
 const Home = () => {
   return (
-    <main className="flex flex-col md:px-10 md:py-8 px-3 py-4">
+    <main className={`flex flex-col md:px-10 md:py-8 px-3 py-4 ${therma.className}`}>
       <Introduction />
       <Links />
       <Mirror />
